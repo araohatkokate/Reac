a6260b0fef255dc30007bbb71a441bbc8e5e0d8a
araohatkokate <araohatk@gmail.com>
2023-10-03 03:38:59 -0500

    updated react app with new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import ListGroup from './components/ListGroup';
 
 function App(){
-  return <div><ListGroup /></div>
+  const items = [
+    'New York',
+    'California',
+    'Texas']
+  return <div><ListGroup items = {items} heading="Cities"/></div>
 }
 
 
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,19 +1,38 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
-function ListGroup() {
+interface Props{
+    items: string[];
+    heading: string;
+}
+
+function ListGroup(props: Props) {
     const items = [
         'New York',
         'California',
         'Texas'
     ];
 
-    
 
+    //Hook - data that will change over time, this lets react know
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+    
+    
+    
+    
+    //Event Handler
     return (
         <Fragment>
             <h1>List</h1>
             <ul className="list-group">
-            {items.map(item => <li>{item}</li>)}
+            {items.map((item, index) => (
+            <li 
+            className={ selectedIndex === index ? 'list-group-item active' : 'list-group-item'}
+            key={item} 
+            onClick = {() => {setSelectedIndex(index);}}
+            >
+            {item}
+            </li>
+            ))}
             </ul>
         </Fragment>
     );
